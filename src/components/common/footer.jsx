6cc2e8fb976b42f0255760699c90d6a747cb91dc
AMD <michojekunle1@gmail.com>
2024-10-03 14:47:19 +0100

    edits to deployment

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -13,14 +13,14 @@ export default function Footer() {
         <div className="max-w-md mx-auto text-center">
           <h2 className="text-2xl font-bold mb-2">Stay connected to us</h2>
           <p className="text-gray-400 mb-6">Subscribe to our newsletter</p>
-          <form onSubmit={handleSubmit} className="flex">
+          <form onSubmit={handleSubmit} className="flex flex-wrap justify-center gap-6 md:gap-4">
             <input
               type="email"
               placeholder="Email address here"
-              className="flex-grow mr-2 bg-white text-black rounded-md px-4 py-1"
+              className="flex-grow bg-white text-black rounded-md px-4 py-2.5"
               required
             />
-            <Button type="submit" className="bg-purple-600 hover:bg-purple-700">
+            <Button type="submit" className="bg-purple-600 w-full sm:w-auto hover:bg-purple-700">
               Submit
             </Button>
           </form>
