615a2d4755a81390de9bc9ffb54d6ca6a93ec55e
AMD <michojekunle1@gmail.com>
2024-10-11 06:22:57 +0100

    edits

--- src/components/courses/index.jsx
+++ src/components/courses/index.jsx
@@ -1,43 +1,49 @@
 /* eslint-disable react/prop-types */
 import { useNavigate } from "react-router-dom";
-import { Button } from "../common"
-import { LockKeyhole } from "lucide-react"
-import { courses } from '../.././pages/data'
-
-
-
+import { Button } from "../common";
+import { LockKeyhole } from "lucide-react";
+import { courses } from "../.././pages/data";
 
 function CourseCard({ title, lessonNumber }) {
-  
-const navigate =useNavigate();
+  const navigate = useNavigate();
 
   function handleCourse() {
-         navigate('/take-course');
+    navigate("/take-course");
   }
   return (
     <div className="bg-pink-50 p-4 rounded-lg shadow-sm">
       <div className="flex justify-between items-center mb-2">
-        <h3 className="text-sm font-medium text-gray-900">Lesson {lessonNumber}</h3>
+        <h3 className="text-sm font-medium text-gray-900">
+          Lesson {lessonNumber}
+        </h3>
         <LockKeyhole className="w-4 h-4 text-gray-500" />
       </div>
       <p className="text-lg font-semibold text-gray-900 mb-4">{title}</p>
-      <Button onClick={handleCourse} className="w-full bg-purple-600 hover:bg-purple-700 text-white">
+      <Button
+        onClick={handleCourse}
+        className="w-full bg-purple-600 hover:bg-purple-700 text-white"
+      >
         Take course
       </Button>
     </div>
-  )
+  );
 }
 
 export default function NewConvertCourses() {
-
   return (
     <div className="max-w-7xl mx-auto px-4 pt-5 md:pt-16 pb-20">
-      <h2 className="text-3xl md:text-4xl font-medium text-purple-800 tracking-wider mb-12 text-center">New Convert Courses</h2>
+      <h2 className="text-3xl md:text-4xl font-medium text-purple-800 tracking-wider mb-12 text-center">
+        New Convert Courses
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10">
         {courses.map((course, index) => (
-          <CourseCard key={index} title={course.title} lessonNumber={course.lessonNumber} />
+          <CourseCard
+            key={index}
+            title={course.title}
+            lessonNumber={course.lessonNumber}
+          />
         ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/courses/test-result.jsx
+++ src/components/courses/test-result.jsx
@@ -1,44 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { congrats } from '../../assets';
-import TextComponent from '../common/text';
-import { Button, ButtonLinks } from '../common';
+import React from "react";
+import { congrats } from "../../assets";
+import TextComponent from "../common/text";
+import { Button, ButtonLinks } from "../common";
 
 const TestResult = () => {
-
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-11'>
-       <div> 
-            <h3 className="text-3xl md:text-4xl font-semibold text-black tracking-wider mb-10 text-center mt-4">Congratulations!!</h3>
-    
-            <div className='flex flex-col items-center justify-center'>
-                
-            <img 
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-11">
+      <div>
+        <h3 className="text-3xl md:text-4xl font-semibold text-black tracking-wider mb-10 text-center mt-4">
+          Congratulations!!
+        </h3>
+
+        <div className="flex flex-col items-center justify-center">
+          <img
             src={congrats}
             alt="congrats img"
             width={100}
             height={100}
             className=""
-            />
+          />
 
-            <h3 className='my-6 text-[#333333]'> You Scored:
-                <span className='text-purple-700 font-semibold'> 60%</span>
-            </h3>
-
-            </div>
-
-            <div  className='flex flex-col items-center justify-center'>
-                <TextComponent className='text-[#474747]' text='Champion!! You are one step closer.' />
-                <TextComponent className='underline ring-offset-4 text-black font-medium cursor-pointer' text='Take next lesson' />
-            </div>
-
-            <div className='flex items-center justify-center m-8'>
-            
-            <ButtonLinks size='md' to='/result-corrections' className="w-96 bg-purple-600 hover:bg-purple-700 text-white  font-light" >View Result</ButtonLinks>
-            </div>
+          <h3 className="my-6 text-[#333333]">
+            {" "}
+            You Scored:
+            <span className="text-purple-700 font-semibold"> 60%</span>
+          </h3>
         </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <TextComponent
+            className="text-[#474747]"
+            text="Champion!! You are one step closer."
+          />
+          <TextComponent
+            className="underline ring-offset-4 text-black font-medium cursor-pointer"
+            text="Take next lesson"
+          />
+        </div>
+
+        <div className="flex items-center justify-center m-8">
+          <ButtonLinks
+            size="md"
+            to="/result-corrections"
+            className="w-96 bg-purple-600 hover:bg-purple-700 text-white  font-light"
+          >
+            View Result
+          </ButtonLinks>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default TestResult;
